157ebb69bbb7af5ee7e0600be3cb777310a17d36
Your Name <you@example.com>
2019-01-17 09:17:05 +0800

    替换功能的构建

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 char arr[1024];
 int index;
+int isMingan(char x)
+{
+    int count = 0;
+    for (int i = 0; 1; i++)
+    {
+        if (x == arr[i])
+        {
+            count++;
+        }
+        if (count == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
 int main()
 {
 
@@ -56,6 +75,26 @@ int main()
         }
         if (code == 4)
         {
+            printf("qingshuruyiduanyingwen \n");
+            char neirong[1024];
+            scanf("%s", neirong);
+            for (int i = 0; 1; i++)
+            {
+                if (neirong[i] == '\0')
+                {
+                    break;
+                }
+                if (isMingan(arr[i]) == 1)
+                {
+                 neirong[i]='*';
+                }
+                printf("%c", neirong[i]);
+            }
+            printf("\n");
+            printf("tihuanchenggong,dianjihuichejixu \n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 5)
         {
